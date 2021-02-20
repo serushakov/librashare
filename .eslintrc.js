f2ade3b3c1cf7457a7b6f8a4d5ec3381e38ffdb0
Sergey Ushakov <sergey@ushakov.io>
2021-02-20 11:53:40 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+};

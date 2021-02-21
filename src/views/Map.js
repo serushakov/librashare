1b0ee7a9d59b35f9a3396c2cfd8a137f5f8b3f89
Sergey Ushakov <sergey@ushakov.io>
2021-02-21 09:29:16 +0200

    created a map component with a map

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,5 +1,49 @@
-import { NativeModules } from 'react-native';
+import MapView from 'react-native-maps';
 
-const Map = () => null;
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+const markers = [
+  {
+    lat: 37,
+    lon: -121,
+  },
+];
+
+const Map = () => {
+  const [camera, setCamera] = useState();
+
+  return (
+    <MapView
+      style={styles.map}
+      camera={camera}
+      // provider="google"
+      onUserLocationChange={(e) => {
+        setCamera(
+          (current) =>
+            current ?? {
+              center: {
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude,
+              },
+              pitch: 0,
+              heading: 0,
+              altitude: 100000,
+              zoom: 7,
+            },
+        );
+      }}
+      showsUserLocation
+      showsMyLocationButton
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;

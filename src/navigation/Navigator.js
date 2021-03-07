8f5fedfd2feedd8c29aa29c439c317e2c80f5d16
Sergey Ushakov <sergey@ushakov.io>
2021-03-07 09:44:49 +0200

    figured out theme for the app

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { ThemeProvider } from 'react-native-elements';
 import React, { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import Login from '../views/Login';
@@ -8,10 +9,17 @@ import BottomTabsNavigator from './BottomTabsNavigator';
 
 const Stack = createStackNavigator();
 
-const Theme = {
+const NavigationTheme = {
   ...DefaultTheme,
+
   colors: {
     ...DefaultTheme.colors,
+    primary: 'rgb(232,100,104)',
+  },
+};
+
+const ElementsTheme = {
+  colors: {
     primary: 'rgb(232,100,104)',
   },
 };
@@ -20,20 +28,22 @@ const Navigator = () => {
   const { isLoggedIn } = useContext(AuthContext);
 
   return (
-    <NavigationContainer theme={Theme}>
-      {isLoggedIn ? (
-        <Stack.Navigator headerMode="none">
-          <Stack.Screen name="Main" component={BottomTabsNavigator} />
-        </Stack.Navigator>
-      ) : (
-        <Stack.Navigator>
-          <>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-          </>
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+    <ThemeProvider theme={ElementsTheme}>
+      <NavigationContainer theme={NavigationTheme}>
+        {isLoggedIn ? (
+          <Stack.Navigator headerMode="none">
+            <Stack.Screen name="Main" component={BottomTabsNavigator} />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <>
+              <Stack.Screen name="Login" component={Login} />
+              <Stack.Screen name="Register" component={Register} />
+            </>
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
